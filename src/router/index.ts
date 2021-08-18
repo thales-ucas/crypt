48b74e1395eded52ef516adffa204e4b90665f3f
源尼玛 <minamoto@sina.com>
2021-08-18 10:46:48 +0800

    hash mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,14 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw } from "vue-router";
 import Base from './children/base';
 
 const routes: Array<RouteRecordRaw> = [
   ...Base
 ];
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
+  
+  routes
 });
 router.beforeEach((to, _from, next) => {
   if (to.meta.title) {
